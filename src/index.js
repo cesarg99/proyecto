45936d38d03cafec184f883fafcdb6fee2350aea
afkJona <JonathanLima04@hotmail.com>
2022-06-07 12:19:32 -0600

    Resolución de conflictos

---
+++ src/index.js
@@ -0,0 +1,22 @@
+var express = require("express")
+var path = require("path")
+var bodyParser = require("body-parser")
+var flash = require("connect-flash")
+var app = express()
+
+
+// Config
+app.set("port", 3000)
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+
+// Middleware.
+// app.use(flash())
+app.use(bodyParser.urlencoded({ extend: false}))
+app.use(bodyParser.json())
+app.use(require("./routes/index.js"))
+
+
+app.listen(app.get("port"), () => {
+    console.log("Corriendo")
+})
